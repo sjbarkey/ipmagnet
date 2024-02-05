fbeeab1127c8f2f9ca35b8971a420e60934e41a7
Steven Barkey <sjbarkey@ucdavis.edu>
2024-02-05 09:14:49 -0800

    Update index.php
    Customized version

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-	$TRACKER=urlencode("http://localhost:80/ipmagnet/"); //Remember to include the trailing slash here!
+    $networkhostname = 'home.barkeyfamily.com';
+	//$TRACKER=urlencode("http://localhost:80/ipmagnet/"); //Remember to include the trailing slash here!
+	$TRACKER=urlencode("http://ipcheckurl.barkeyfamily.com:80/ipcheck/"); //Remember to include the trailing slash here!
+	//$TRACKER=urlencode("http://steven.16mb.com:80/ipcheck/"); //Remember to include the trailing slash here!
+	//$TRACKER=urlencode("http://stevenbarkey.getenjoyment.net:80/ipcheck/"); //Remember to include the trailing slash here!
 	$db = new PDO("sqlite:ipmagnet.db3");
 	$enableInterval=false;
 	$trackerInterval=300;
@@ -10,7 +14,14 @@
 		return preg_match('/^[A-Fa-f0-9]{40}$/', $string);
 	}
 
+	//http://stevenbarkey.getenjoyment.net/ipcheck/?hash=cf0e2da00764c0128771433e46d2838a0d64dc35
 	//BitTorrent clients will submit the info_hash parameter when requesting the magnet link
+	
+    if(isset($_GET["network"])){
+		$returnedip = gethostbyname($_GET["network"]);				
+        $networkip = $returnedip;
+	}
+
 	if(isset($_GET["info_hash"])){
 
 		//prepare the insert query
@@ -32,7 +43,22 @@
 		if(isset($_GET["ip"])&&$_GET["ip"]!=$_SERVER["REMOTE_ADDR"]){
 			$addrs.=", ".htmlentities($_GET["ip"], ENT_QUOTES);
 		}
-
+                
+                //update the Google DDNS domain with the new IP Address
+                //file_get_contents('https://DFuk9pDz7YpJIUIi:EdW74LSIbcvo4bao@domains.google.com/nic/update?hostname=ddnstor.barkeyfamily.com&myip='.$addrs);
+                //$curl = curl_init();
+                //curl_setopt ($curl, CURLOPT_URL, "https://DFuk9pDz7YpJIUIi:EdW74LSIbcvo4bao@domains.google.com/nic/update?hostname=ddnstor.barkeyfamily.com&myip=5.5.5.5");
+                //curl_exec ($curl);
+                //curl_close ($curl);
+               
+                if ($addrs != gethostbyname($networkhostname)){
+                        $reportedaddrs = $addrs." (VPN Active)";
+                        $addrs = '<font color=\'green\'>'.$addrs.'</font>';
+                }else{
+                        $reportedaddrs = $addrs." (WARNING - VPN Inactive)";
+                        $addrs = '<font color=\'red\'>'.$addrs.'</font>';
+                }
+                
 		//insert the hit into the database
 		if(!($stmt->execute(
 			array(
@@ -48,7 +74,7 @@
 		$stmt->closeCursor();
 
 		//print the ips as "failure reason" to be displayed by some clients
-		$resp="IP: ".$addrs;
+		$resp="IP: ".$reportedaddrs;
 		$resp="14:failure reason".strlen($resp).":".$resp;
 
 		if($enableInterval){
@@ -73,7 +99,26 @@
 	}
 
 	$returnValue["hash"]=$HASH;
+	
+	//delete old hits from the database
+	if(isset($_GET["cleanupdb"])){
+		$cleartime = time() - (12 * 60 * 60);
+		$query="DELETE FROM hits WHERE timestamp < ".$cleartime."";
+		//echo $query."<br />".time();;
+		$stmt=$db->prepare($query);
 
+		if($stmt===FALSE){
+			$returnValue["message"]="Failed to prepare query.";
+			$returnValue["code"]=2;
+			exit(json_encode($returnValue));
+		}
+
+		$stmt->execute();
+		
+		$stmt->closeCursor();
+	}
+
+	//delete all hits for a hash from the database
 	if(isset($_GET["clear"])){
 		//delete all hits for a hash from the database
 		$query="DELETE FROM hits WHERE hash=:hash";
@@ -114,6 +159,7 @@
 	if(isset($_GET["ajax"])){
 		//fetch all hits into one array
 		$returnValue["hits"]=$stmt->fetchAll(PDO::FETCH_ASSOC);
+		//asort($stmt);
 		$stmt->closeCursor();
 
 		//set content-type and CORS headers
@@ -126,29 +172,49 @@
 <?php echo '<?xml version="1.1" encoding="UTF-8" ?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>ipMagnet</title>
+        <head>
+		<title>ipCheck</title>
 		<link rel="icon" href="static/favicon.png" type="image/x-icon" />
 		<link rel="stylesheet" type="text/css" href="static/ipmagnet.css" />
-		<script type="text/javascript" src="static/ajax.js"></script>
+		<!--
+                <script type="text/javascript" src="static/ajax.js"></script>
 		<script type="text/javascript" src="static/ipmagnet.js"></script>
-		<meta name="robots" content="noindex,nofollow" />
+		-->
+                <script type="text/javascript">
+                function vpnVerify(ip, status){
+                        document.getElementById("vpn-ip").innerHTML = ip;
+                        document.getElementById("vpn-status").innerHTML = status;
+                }
+                </script>
+                <meta name="robots" content="noindex,nofollow" />
 		<noscript>
 			<meta http-equiv="refresh" content="60; url=?hash=<?php print($HASH); ?>" />
 		</noscript>
 	</head>
 	<body onload="ipmagnet.init('<?php print($HASH); ?>');">
 		<div id="title-wrap">
-			<h1>ipMagnet</h1>
+			<h1>ipCheck</h1>
 		</div>
 		<div id="center-wrap">
 			<div id="content-main">
 				<div id="mission-statement">
-					ipMagnet allows you to see which IP address your BitTorrent Client is handing out to its peers and trackers!
+					ipCheck allows you to see which IP address your BitTorrent Client is handing out to its peers and trackers!
 				</div>
-				Add this <a href="magnet:?xt=urn:btih:<?php print($HASH); ?>&amp;dn=ipMagnet+Tracking+Link&amp;tr=<?php print($TRACKER); ?>">Magnet link</a> to your 
-				downloads and watch this page.<br/>
-				FYI, the address you've accessed this page with is <span id="remote-ip"><?php print($_SERVER["REMOTE_ADDR"]); ?></span>
+				Add this <a href="magnet:?xt=urn:btih:<?php print($HASH); ?>&amp;dn=ipCheck+Tracking+Link&amp;tr=<?php print($TRACKER); ?>">Magnet link</a> to your client and watch this page.<br/>
+				<?php
+                                $browseraddress = ($_SERVER["REMOTE_ADDR"]);
+				$browseraddress = substr($browseraddress,0,strlen($browseraddress));
+				?>
+                The address you've accessed this page with is: <span id="remote-ip"><a href="http://www.ipgeek.net/<?php echo $browseraddress; ?>" target=\"_blank\"><?php echo $browseraddress; ?></a></span><br/><br/>
+                <?php
+                if(isset($_GET["network"])){			
+					echo "<span id=\"network-ip\"><a href=\"http://www.ipgeek.net/".$networkip."\" target=\"_blank\">".$networkip."</span></a>: CURRENT IP Address for '".$_GET["network"]."'<br/>";
+				}		
+				echo "<span id=\"vpn-ip\"></span>: LATEST reported IP Address<br />";
+                if(isset($_GET["network"])){
+                	echo "<span id=\"vpn-status\"></span><br />";
+              	}
+                                ?>
 				<div id="current-connections">
 					<div id="app-links">
 						<a href="?hash=<?php print($HASH); ?>" class="app-link" id="update-link">Update</a>
@@ -156,33 +222,66 @@
 					</div>
 					<table id="conn-table">
 						<tr>
-							<th>Timestamp</th>
-							<th>IP address(es)</th>
-							<th>User Agent</th>
+							<th align="center">Timestamp</th>
+							<th align="center">IP address(es)</th>
+							<th align="center">Torrent Client</th>
 						</tr>
 						<?php
+							//sort($stmt);
 							$row=$stmt->fetch(PDO::FETCH_ASSOC);
 							while($row!==FALSE){
+								$rowip=$row["addr"];
+                                $rowip = str_replace('</font>', '', $rowip);
+								$rowip = str_replace('<font color=\'red\'>', '', $rowip);
+								$rowip = str_replace('<font color=\'green\'>', '', $rowip);
 								print("<tr>");
-									print("<td>".date("d.m.Y H:i:s (e)",$row["timestamp"])."</td>");
-									print("<td>".$row["addr"]."</td>");
-									print("<td>".$row["agent"]."</td>");
+									print("<td align=\"center\">".date("Y-m-d H:i:s",$row["timestamp"] - 60 * 60 * 7)."</td>");
+                                    print("<td align=\"center\"><a href=\"http://www.ipgeek.net/".$rowip."\" target=\"_blank\">".$row["addr"]."</a></td>");
+                                    print("<td align=\"center\">".$row["agent"]."</td>");
 								print("</tr>");
+								if(isset($_GET["network"])){
+									$vpnstatus = "<font color=\"red\">INACTIVE</font>";
+									if (gethostbyname($_GET["network"]) != $row["addr"]){
+										$vpnstatus = "<font color=\"green\">ACTIVE</font>";
+									}
+								}
+                                                                $latestip = $row["addr"];
 								$row=$stmt->fetch(PDO::FETCH_ASSOC);
 							}
 							$stmt->closeCursor();
-						?>
+						
+                                                        if(isset($_GET["network"])){        
+                                                                echo '<script type="text/javascript">';
+                                                                        //echo 'vpnVerify("'.str_replace("green","black",$latestip).'");';
+                                                                        $strippedip = substr($latestip,20, strlen($latestip)-27);
+																		//$strippedip = substr($strippedip, 0, strlen($strippedip)-7);
+																		if(stristr($latestip, $networkip)){
+                                                                                echo 'vpnVerify("<a href=\'http://www.ipgeek.net/'.$strippedip.'\' target=\'_blank\'>'.$latestip.'</a>", "<font color=\"red\">WARNING: The VPN is currently inactive!</font>");';
+                                                                        } else {
+                                                                                echo 'vpnVerify("<a href=\'http://www.ipgeek.net/'.$strippedip.'\' target=\'_blank\'>'.str_replace("green","black",$latestip).'</a>", "<font color=\"green\">The VPN is currently active.</font>");';
+																				//echo 'vpnVerify("<a href=\'http://www.ipgeek.net/'.$strippedip.'\' target=\'_blank\'>'.$latestip.'</a>", "<font color=\"green\">The VPN is currently active.</font>");';
+                                                                        }
+                                                                echo '</script>';
+                                                        }
+                                                ?>
 					</table>
 				</div>
 			</div>
-
+                       
 			<div id="footer-text">
 			<span id="status-line">Status: <span id="status-text">Using plain HTML</span></span>
 				<span id="meta-footer">
-					Proudly run without ads or web tracking. Set up your own with the
+					<?php
+					if(isset($vpnstatus)){
+						echo "<b><font color=\"black\">VPN Status: </font>".$vpnstatus."</b>&nbsp;";
+					}
+					?>
+                    <!--
+                    Proudly run without ads or web tracking. Set up your own with the
 					<a href="https://github.com/cbdevnet/ipmagnet">[source]</a>
 					<a href="http://www.kopimi.com/kopimi/"><img src="static/kopimi.png" alt="kopimi"/></a>
 					<a href="http://wtfpl.net/"><img src="static/wtfpl.png" alt="wtfpl"/></a>
+                    -->
 				</span>
 			</div>
 		</div>
